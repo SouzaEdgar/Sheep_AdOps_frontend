08e3fa681aa55da94a5f841904275bff564bd195
SouzaEdgar <edgarss2013@gmail.com>
2025-09-22 16:25:02 -0300

    refactor: rota post

--- scripts/script.js
+++ scripts/script.js
@@ -51,7 +51,7 @@ form.addEventListener("submit", async (event) => {
 
     try {
         const response = await fetch(API_URL, {
-            method: "GET",
+            method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ urls, parametros })
         });
